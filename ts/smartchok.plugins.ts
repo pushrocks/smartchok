5e5f2ce6c2e94e73140c554dcfdb5d6c31e9813c
Phil Kunz <gitlab@philkunz.com>
2017-06-30 18:06:37 +0200

    clean up code

--- ts/smartchok.plugins.ts
+++ ts/smartchok.plugins.ts
@@ -10,6 +10,6 @@ import {Operator} from 'rxjs/Operator';
 import {Observable} from 'rxjs/Observable';
 declare module 'rxjs/Subject' {
   interface Subject<T> {
-    lift<R>(operator: Operator<T, R>): Observable<R>;
+    lift<R>(operator: Operator<T, R>): Observable<R>
   }
 }

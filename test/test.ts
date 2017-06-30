ab3ad55c43c726e312fc44d449baf489d1b845f2
Phil Kunz <gitlab@philkunz.com>
2017-06-30 18:12:00 +0200

    fix loading of rxjs

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,11 @@
 import { tap, expect } from 'tapbundle'
 import * as smartfile from 'smartfile'
 import * as smartq from 'smartq'
-import * as rx from 'rxjs/Rx'
 
 // the module to test
 import * as smartchok from '../dist/index'
+
+import * as rx from 'rxjs/Rx'
 
 let testSmartchok: smartchok.Smartchok
 let testAddObservable: rx.Observable<any>

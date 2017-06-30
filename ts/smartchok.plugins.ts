46e7b52930677c43828e97d74c549a8405f34664
Phil Kunz <gitlab@philkunz.com>
2017-06-30 18:16:59 +0200

    update

--- ts/smartchok.plugins.ts
+++ ts/smartchok.plugins.ts
@@ -6,8 +6,8 @@ export import q = require('q')
 export import rx = require('rxjs/Rx')
 
 // workaround
-import {Operator} from 'rxjs/Operator';
-import {Observable} from 'rxjs/Observable';
+import {Operator} from 'rxjs/Operator'
+import {Observable} from 'rxjs/Observable'
 declare module 'rxjs/Subject' {
   interface Subject<T> {
     lift<R>(operator: Operator<T, R>): Observable<R>
